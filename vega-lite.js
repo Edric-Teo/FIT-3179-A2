17f6ecf27d43b43847356320d2f07e3c9e1fdac3
edric <eteo0025@student.monash.edu>
2025-10-16 17:13:53 +0800

    Added line chart about tournament prize pool

--- vega-lite.js
+++ vega-lite.js
@@ -1,4 +1,14 @@
 var vg_1 = "tourney_hosts.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#tourney_map", vg_1).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "prize_pool.json";
+vegaEmbed("#prizepool_linechart", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+var vg_3 = "HLTV_ratings.json";
+vegaEmbed("#hltv_radarchart", vg_3).then(function(result) {
 
 }).catch(console.error);
